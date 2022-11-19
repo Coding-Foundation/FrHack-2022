93232d8dc06450c7fb146e9179da9215bbddeb57
Crakleurs <eti.faviere@gmail.com>
2022-11-19 20:42:15 +0100

    init front

---
+++ anfr-front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainMap from "./components/Map";
+
+function App() {
+  return (
+    <div className="App">
+      <MainMap/>
+    </div>
+  );
+}
+
+export default App;
---
+++ anfr-front/src/components/Map.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const MainMap = () => {
+
+    const position = {
+        lng: 2.213749,
+        lat: 46.227638
+    }
+
+    return (
+        <MapContainer center={position} zoom={6} scrollWheelZoom={false} className={"map-container"}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+
+export default MainMap;
