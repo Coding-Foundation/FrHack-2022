d6fb9ce9c42d955bb0f2e323c880ff40f585726a
Crakleurs <eti.faviere@gmail.com>
2022-11-19 19:03:04 +0100

    models api

--- anfr-api/app.py
+++ anfr-api/app.py
@@ -1,13 +1,29 @@
 from typing import Union
-
 from fastapi import FastAPI
+from database import SessionLocal
+from import_service import ImportService
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
+
+database = get_db()
+
+importService = ImportService(database)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/import")
+def importDB():
+    return importService.importAntenna()
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
--- anfr-api/models.py
+++ anfr-api/models.py
@@ -1,25 +1,73 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Float, Column, ForeignKey, Date, Integer, String
 from sqlalchemy.orm import relationship
-
 from .database import Base
 
-class User(Base):
-    __tablename__ = "users"
+
+class SystemTelecom(Base):
+    __tablename__ = "system_telecom"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    generation = Column(String, index=True)
+    operator = Column(String, index=True)
+    transmiters = relationship("Transmitter", back_populates="system")
+
+
+
+class Transmitter(Base):
+    __tablename__ = "transmitter"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    creation_date = Column(String, index=True)
+    system = Column(Integer, ForeignKey("system_telecom.id"))
+    antenna = Column(Integer, ForeignKey("antenna.id"))
+    position = Column(Integer, ForeignKey("position.id"))
+    captors = relationship("Captor", back_populates="transmitter")
 
-    items = relationship("Item", back_populates="owner")
 
-
-class Item(Base):
-    __tablename__ = "items"
+class Antenna(Base):
+    __tablename__ = "antenna"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    azimut = Column(Integer, index=True)
+    altitude = Column(Integer, index=True)
+    transmitter = relationship("Transmitter", back_populates="antenna")
+
+
+class Position(Base):
+    __tablename__ = "position"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code_insee = Column(Integer, index=True)
+    code_postal = Column(Integer, index=True)
+    departement = Column(Integer, index=True)
+    lib_dpt = Column(String, index=True)
+    code_region = Column(Integer, index=True)
+    lib_region = Column(String, index=True)
+    lib_maj_reg = Column(String, index=True)
+    altitude = Column(Integer, index=True)
     description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    latitude = Column(Float, index=True)
+    longitude = Column(Float, index=True)
 
-    owner = relationship("User", back_populates="items")
+
+class Captor(Base):
+    __tablename__ = "captor"
+
+    id = Column(Integer, primary_key=True, index=True)
+    creation_date = Column(String, index=True)
+    name = Column(String, unique=True, index=True)
+    address = Column(String, index=True)
+    code_postal = Column(String, index=True)
+    latitude = Column(Float, index=True)
+    longitude = Column(Float, index=True)
+    measures = relationship("Measure", back_populates="captor")
+    transmitter = Column(Integer, ForeignKey("transmitter.id"))
+
+
+class Measure(Base):
+    __tablename__ = "measure"
+
+    id = Column(Integer, primary_key=True, index=True)
+    value = Column(Float, index=True)
+    date = Column(Date, index=True)
+    captor = Column(Integer, ForeignKey("captor.id"))
