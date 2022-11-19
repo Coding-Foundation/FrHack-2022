5fee30d6fb5d0a75a41db3f5a45a6e6d70ef7b76
Marc Partensky <marc.partensky@gmail.com>
2022-11-20 00:48:33 +0100

    alexandre + copy

---
+++ prep/alextools.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def savePlotofVector(vec, filename, dataset, title=""):
+    plt.figure(figsize=(30, 4))
+    plt.bar(range(len(vec)), vec)
+    plt.xticks(range(len(vec)), dataset.X_week_labels(), rotation=90, fontsize=20)
+    plt.title(title)
+    plt.savefig(f'{filename}.png', bbox_inches="tight", dpi=300)
+    plt.show()
