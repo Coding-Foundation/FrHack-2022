75642ba1d1dea2d96ac0e0f89f4d0f4765b084be
Marc Partensky <marc.partensky@gmail.com>
2022-11-20 02:35:47 +0100

    alex

--- prep/alextools.py
+++ prep/alextools.py
@@ -7,3 +7,14 @@ def savePlotofVector(vec, filename, dataset, title="", color="b"):
     plt.title(title)
     plt.savefig(f'{filename}.png', bbox_inches="tight", dpi=300)
     plt.show()
+
+def show7dayFromDatetime(dataset, sonde:str, i_datetime):
+    select = dataset.measure_df[(dataset.measure_df["numero"]==sonde) & (dataset.measure_df["datetime"]>=i_datetime)].iloc[:12*7]
+    vec = select["E_volt_par_metre"].iloc[:12*7].to_numpy()
+    labels = tuple(select["date"])
+    
+    plt.figure(figsize=(30, 4))
+    plt.bar(range(len(vec)), vec, color="g")
+    plt.xticks(range(len(vec)), labels, rotation=90, fontsize=20)
+    plt.title("")
+    plt.show()
