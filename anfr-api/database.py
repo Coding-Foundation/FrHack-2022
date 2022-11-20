1e61b1226ed90785f6c8c7e5e944b98516c01854
Marc Partensky <marc.partensky@gmail.com>
2022-11-20 04:29:51 +0100

    fuck passwords

--- anfr-api/database.py
+++ anfr-api/database.py
@@ -1,9 +1,10 @@
 import psycopg2
+import os
 
-HOST = "marcpartensky.com"
-PORT = "5433"
-DATABASE = "db"
-USER = "user"
-PASSWORD = "password"
+HOST = os.environ.get("POSTGRES_HOST") or "marcpartensky.com"
+PORT = os.environ.get("POSTGRES_PORT") or "5433"
+DATABASE = os.environ.get("POSTGRES_DB") or "db"
+USER = os.environ.get("POSTGRES_USER") or "user"
+PASSWORD = os.environ.get("POSTGRES_PASSWORD") or "password"
 
 conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s port=%s" % (HOST, DATABASE, USER, PASSWORD, PORT))
