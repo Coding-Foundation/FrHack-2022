82767dbb7d5e0d4350225294695ace1fb17633da
Crakleurs <eti.faviere@gmail.com>
2022-11-20 13:15:22 +0100

    fix api

--- anfr-api/app.py
+++ anfr-api/app.py
@@ -42,33 +42,43 @@ def read_root():
 
 @app.get("/clusters")
 async def getClusters():
-    cur = conn.cursor(cursor_factory=RealDictCursor)
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
 
-    sql = "SELECT * FROM captor_cluster"
-    cur.execute(sql)
-    results = cur.fetchall()
-    cur.close()
-    return results
+        sql = "SELECT * FROM captor_cluster"
+        cur.execute(sql)
+        results = cur.fetchall()
+        cur.close()
+        return results
+    except Exception:
+        print("Erreur")
+
+
 @app.get("/captors")
 def getCaptors():
-    cur = conn.cursor(cursor_factory=RealDictCursor)
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        sql = "SELECT * FROM captor FULL JOIN captor_cluster ON captor.name = captor_cluster.numero"
+        cur.execute(sql)
+        result = cur.fetchall()
+        cur.close()
+        return result
 
-    sql = "SELECT * FROM captor FULL JOIN captor_cluster ON captor.name = captor_cluster.numero"
-    cur.execute(sql)
-    result = cur.fetchall()
-    cur.close()
-    return result
+    except Exception:
+        print("Erreur")
 
 
 @app.get("/captors/{id}")
 def getCaptorsResults(id: int):
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-
-    sql = "SELECT * FROM captor_cluster WHERE " + id
-    cur.execute(sql)
-    result = cur.fetchone()
-    cur.close()
-    return result
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        sql = "SELECT * FROM captor_cluster WHERE " + id
+        cur.execute(sql)
+        result = cur.fetchone()
+        cur.close()
+        return result
+    except Exception:
+        print("Erreur")
 
 
 @app.get("/antennas")
@@ -83,21 +93,27 @@ def getAntennas():
 
 @app.get("/antennas/{id}")
 def getAntennas(id: int):
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    sql = "SELECT * FROM antenna WHERE id = " + str(id)
-    cur.execute(sql)
-    results = cur.fetchone()
-    cur.close()
-    return results
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        sql = "SELECT * FROM antenna WHERE id = " + str(id)
+        cur.execute(sql)
+        results = cur.fetchone()
+        cur.close()
+        return results
+    except Exception:
+        print("Erreur")
 
-@app.get("/emitters/{id_antenna}")
-def getAntennas(id_antenna: int):
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    sql = "SELECT * FROM transmitter WHERE antenna = " + str(id_antenna)
-    cur.execute(sql)
-    results = cur.fetchall()
-    cur.close()
-    return results
+@app.get("/transmitters/{id_antenna}")
+def getTransmitters(id_antenna: int):
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        sql = "SELECT * FROM transmitter WHERE antenna = " + str(id_antenna)
+        cur.execute(sql)
+        results = cur.fetchall()
+        cur.close()
+        return results
+    except Exception:
+        print("Erreur")
 
 
 @app.get("/results/{name}")
