205292de6d41e6ce30ae8ed7b02758f6130f615f
Marc Partensky <marc.partensky@gmail.com>
2022-11-20 04:36:34 +0100

    swagger

--- anfr-api/app.py
+++ anfr-api/app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from psycopg2.extras import RealDictCursor
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
+from fastapi.openapi.utils import get_openapi
 from database import conn
 
 
@@ -17,6 +18,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 @app.get("/")
 def read_root():
