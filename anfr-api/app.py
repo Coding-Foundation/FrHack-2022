bcf82789fab28bc27c440150724e144eda9071ab
efaviere <eti.faviere@gmail.com>
2022-11-20 01:16:06 +0100

    edit image expose

--- anfr-api/app.py
+++ anfr-api/app.py
@@ -13,33 +13,38 @@ database = SessionLocal()
 
 importService = ImportService(database)
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/captors")
 def getCaptors():
-    #Récupérer tous les capteurs, y ajouter aussi leurs profil type
+    # Récupérer tous les capteurs, y ajouter aussi leurs profil type
     return database.query(Captor).all()
+
 
 @app.get("/captors/{id}")
 def getCaptorsResults(id: int):
-    #Récupérer les informations du capteur ainsi que les résultats
+    # Récupérer les informations du capteur ainsi que les résultats
     return database.query(Captor).get(id)
+
 
 @app.get("/antennas")
 def getAntennas():
-    #Récupérer toutes les antennes
+    # Récupérer toutes les antennes
     return database.query(Antenna).all()
 
 
 @app.get("/antennas/{id}")
 def getAntennas(id: int):
-    #Récupérer toutes les antennes
+    # Récupérer toutes les antennes
     return database.query(Antenna).get(id)
+
 
 @app.get("/results/{name}")
 def getResults(name: str):
     decoded_string = base64.b64decode(name)
     image_name = decoded_string.decode("utf-8")
-    return FileResponse("pictures/" + image_name + ".png")
+    return FileResponse("../prep/week_derive_plots/" + image_name + ".png")
