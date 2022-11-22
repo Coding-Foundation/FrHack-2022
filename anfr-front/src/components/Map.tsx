d9f1e73d7bf0dba203a2dfe8585b0cd3815e2ba6
Crakleurs <eti.faviere@gmail.com>
2022-11-22 16:09:37 +0100

    change map size on mobile

--- anfr-front/src/components/Map.tsx
+++ anfr-front/src/components/Map.tsx
@@ -44,7 +44,7 @@ const MainMap: React.FC<Props> = (props) => {
   })
 
     return (
-    <MapContainer center={position} zoom={6} className={"w-full h-screen md:h-full"}>
+    <MapContainer center={position} zoom={6} className={"w-full h-[80vh] md:h-full"}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
