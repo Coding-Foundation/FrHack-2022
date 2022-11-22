a2f2d62d5e4936b9e6dacd39f1e7b903fede2824
Crakleurs <eti.faviere@gmail.com>
2022-11-22 16:00:54 +0100

    beatifull UI

--- anfr-front/src/App.tsx
+++ anfr-front/src/App.tsx
@@ -7,23 +7,23 @@ import {Antenna} from "./types/Antenna";
 import {Captor} from "./types/Captor";
 
 function App() {
-  const [selectedAntenna, setSelectedAntenna]  = useState<Antenna | null>(null);
-  const [selectedCaptor, setSelectedCaptor]  = useState<Captor | null>(null);
+    const [selectedAntenna, setSelectedAntenna] = useState<Antenna | null>(null);
+    const [selectedCaptor, setSelectedCaptor] = useState<Captor | null>(null);
 
-  const selectObject = (antenna: Antenna | null, captor: Captor | null) => {
-    setSelectedAntenna(antenna);
-    setSelectedCaptor(captor);
-  }
+    const selectObject = (antenna: Antenna | null, captor: Captor | null) => {
+        setSelectedAntenna(antenna);
+        setSelectedCaptor(captor);
+    }
 
-  return (
-    <div className={"flex flex-col h-screen max-h-screen"}>
-      <NavBar/>
-      <div className={"flex flex-col md:flex-row grow"}>
-        <MainMap selectObject={selectObject}/>
-        <SideAlert antenna={selectedAntenna} captor={selectedCaptor}/>
-      </div>
-    </div>
-  );
+    return (
+        <div className={"flex flex-col min-h-screen w-full md:h-screen md:max-h-screen"}>
+            <NavBar/>
+            <div className={"flex flex-col h-full w-full md:flex-row md:max-h-full"}>
+                <MainMap selectObject={selectObject}/>
+                <SideAlert antenna={selectedAntenna} captor={selectedCaptor}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
