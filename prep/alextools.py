91e5099f51679313fa1555a3af917940a109453d
Marc Partensky <marc.partensky@gmail.com>
2022-11-28 12:20:32 +0100

    prep d'alex

--- prep/alextools.py
+++ prep/alextools.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
-def savePlotofVector(vec, filename, dataset, title="", color="b"):
+def savePlotofVector(vec, filename, dataset, title="", color="b", save=True):
     plt.figure(figsize=(30, 4))
     plt.bar(range(len(vec)), vec, color=color)
     plt.xticks(range(len(vec)), dataset.X_week_labels(), rotation=90, fontsize=20)
     plt.title(title)
-    plt.savefig(f'{filename}.png', bbox_inches="tight", dpi=300)
+    if save:
+        plt.savefig(f'{filename}.png', bbox_inches="tight", dpi=300)
     plt.show()
 
 def show7dayFromDatetime(dataset, sonde:str, i_datetime):
