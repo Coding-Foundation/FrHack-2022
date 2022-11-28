8cccb39a2a49d83d7533f3cea8ab1eb9189829b0
Marc Partensky <marc.partensky@gmail.com>
2022-11-28 14:57:55 +0100

    init.sh

---
+++ init.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# Credits
+# echo "Credits to : Marc Partensky, Alexandre Bigot et Étienne Favière"
+# echo --------
+# echo
+
+env_path=./env
+mkdir -p $env_path
+
+# Helper functions
+random() {
+    echo "$RANDOM `date +%s`" | sha256sum | base64 | head -c 32
+}
+scrap_domain_name() {
+    dig -x `curl -sLq https://ipconfig.io` | grep PTR | grep -v ";" | awk '{print $NF}' | sed 's/.$//'
+}
+
+
+# Default values
+domain_name=`scrap_domain_name`
+postgres_user=`random`
+postgres_password=`random`
+postgres_db=frhack
+postgres_host=postgres_frhack
+postgres_port=5432
+registry=marcpartensky
+node_env=production
+front_port=80
+public_postgres_port=5432
+network_name=web
+
+# Prompt
+read -e -p "- Domain name: " -i $domain_name domain_name
+read -e -p "- PostgreSQL public port: " -i $public_postgres_port public_postgres_port
+read -e -p "- PostgreSQL username: " -i $postgres_user postgres_user
+read -e -p "- PostgreSQL password: " -i $postgres_password postgres_password
+read -e -p "- PostgreSQL database name: " -i $postgres_db postgres_db
+read -e -p "- Node env: " -i $node_env node_env
+read -e -p "- Docker registry: " -i $registry registry
+read -e -p "- Docker external network name: " -i $network_name network_name
+
+# Output to .env
+echo "# $env_path/postgres.env
+POSTGRES_USER=$postgres_user
+POSTGRES_DB=$postgres_db
+POSTGRES_PASSWORD=$postgres_password
+" > $env_path/postgres.env
+
+echo "# $env_path/api.env
+POSTGRES_USER=$postgres_user
+POSTGRES_DB=$postgres_db
+POSTGRES_PASSWORD=$postgres_password
+POSTGRES_PORT=$postgres_port
+POSTGRES_HOST=$postgres_host
+" > $env_path/api.env
+
+echo "# $env_path/pgweb.env
+DATABASE_URL=postgres://$postgres_user:$postgres_password@$postgres_host:$postgres_port/$postgres_db?sslmode=disable
+" > $env_path/pgweb.env
+
+echo "# $env_path/front.env
+NODE_ENV=$node_env
+FRONT_PORT=80
+API_URL=api.$domain_name
+" > $env_path/front.env
+
+echo "# $env_path/docker-compose.env
+REGISTRY=$registry
+DOMAIN_NAME=$domain_name
+PUBLIC_POSTGRES_PORT=$public_postgres_port
+NETWORK_NAME=$network_name
+" > $env_path/docker-compose.env
+
+# Display
+echo 
+echo --------
+echo 
+
+cat env/*
--- makefile
+++ makefile
@@ -1,6 +1,44 @@
+# Credits
+
+STACK_NAME=frhack
+
+credits:
+	@echo ---------------------------------------------------------------
+	@echo Credits to : Marc Partensky, Alexandre Bigot et Étienne Favière
+	@echo ---------------------------------------------------------------
+	@echo 
+
+compose: setup
+	docker-compose up -d
+swarm: setup
+	$(call setup_env,docker-compose)
+	docker stack deploy -c docker-compose.yml $(STACK_NAME)
+
+setup: credits env build
+build: env
+	docker-compose build
+
+rm-stack:
+	docker stack rm $(STACK_NAME)
+
+env:
+	./init.sh
+clear:
+	rm -rf ./env
+reset: clear env
+
 lab:
 	jupyter-lab --ip 0.0.0.0 --collaborative --allow-root
-front-install:
-	npm --prefix anfr-front install
-front:
+front: anfr-front/node_modules
 	npm --prefix anfr-front start
+anfr-front/node_modules:
+	npm --prefix anfr-front install
+
+# helper
+define setup_env
+	$(eval ENV_FILE := env/$(1).env)
+	@echo "Load $(ENV_FILE)"
+	$(eval include env/$(1).env)
+	$(eval export sed 's/=.*//' env/$(1).env)
+endef
+
